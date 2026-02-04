2bd2e814812803c536005e7b65ea687143c6e15a
GH Hwang <popprik4@gmail.com>
2026-02-04 18:10:04 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/com/smati/smati_backend/SmatiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smati.smati_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmatiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
